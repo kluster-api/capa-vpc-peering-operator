c216894373f9f9a65282d119370e73aaf76aba59
Rasel Hossain <59439107+Superm4n97@users.noreply.github.com>
2023-06-27 14:40:46 +0600

    Fix resource not found error (#2)
    Signed-off-by: rasel <rasel@appscode.com>

--- pkg/firewall/crossplane.go
+++ pkg/firewall/crossplane.go
@@ -37,10 +37,10 @@ type RouteInfo struct {
 	RouteTable, Destination, Region, PeeringConnectionID string
 }
 
-func getRoute(routeInfo RouteInfo, ownerRef []metav1.OwnerReference) *ec2api.Route {
+func GetRoute(routeInfo RouteInfo, ownerRef []metav1.OwnerReference) *ec2api.Route {
 	route := ec2api.Route{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:            generateRouteName(routeInfo.RouteTable, routeInfo.Destination),
+			Name:            GetRouteName(routeInfo.RouteTable, routeInfo.Destination),
 			OwnerReferences: ownerRef,
 		},
 		Spec: ec2api.RouteSpec{
@@ -59,7 +59,7 @@ type RuleInfo struct {
 	DestinationCidr, Region, SecurityGroup, ToPort, FromPort string
 }
 
-func getRule(ruleInfo RuleInfo, ownerRef []metav1.OwnerReference) (*ec2api.SecurityGroupRule, error) {
+func GetRule(ruleInfo RuleInfo, ownerRef []metav1.OwnerReference) (*ec2api.SecurityGroupRule, error) {
 	var rule ec2api.SecurityGroupRule
 	toPort, err := strconv.ParseFloat(ruleInfo.ToPort, 64)
 	if err != nil {
@@ -76,7 +76,7 @@ func getRule(ruleInfo RuleInfo, ownerRef []metav1.OwnerReference) (*ec2api.Secur
 
 	rule = ec2api.SecurityGroupRule{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:            generateRuleName(ruleInfo.SecurityGroup, ruleInfo.DestinationCidr),
+			Name:            GetSGRuleName(ruleInfo.SecurityGroup, ruleInfo.DestinationCidr),
 			OwnerReferences: ownerRef,
 		},
 		Spec: ec2api.SecurityGroupRuleSpec{
@@ -94,8 +94,8 @@ func getRule(ruleInfo RuleInfo, ownerRef []metav1.OwnerReference) (*ec2api.Secur
 	return &rule, nil
 }
 
-func CreateOrPatchRule(ctx context.Context, c client.Client, info RuleInfo, ownerRef []metav1.OwnerReference) error {
-	sgRule, err := getRule(info, ownerRef)
+func CreateSecurityGroupRule(ctx context.Context, c client.Client, info RuleInfo, ownerRef []metav1.OwnerReference) error {
+	sgRule, err := GetRule(info, ownerRef)
 	if err != nil {
 		return err
 	}
@@ -110,8 +110,8 @@ func CreateOrPatchRule(ctx context.Context, c client.Client, info RuleInfo, owne
 	return nil
 }
 
-func CreateOrPatchRoute(ctx context.Context, c client.Client, info RouteInfo, ownerRef []metav1.OwnerReference) error {
-	route := getRoute(info, ownerRef)
+func CreateRouteTableRoute(ctx context.Context, c client.Client, info RouteInfo, ownerRef []metav1.OwnerReference) error {
+	route := GetRoute(info, ownerRef)
 	_, _, err := kmc.CreateOrPatch(ctx, c, route, func(_ client.Object, _ bool) client.Object {
 		return route
 	})
@@ -122,4 +122,8 @@ func CreateOrPatchRoute(ctx context.Context, c client.Client, info RouteInfo, ow
 	klog.Infof("route created to table %s for %s", info.RouteTable, info.Destination)
 
 	return nil
+}
+
+type VPCIdentifier struct {
+	Name, Cidr string
 }
